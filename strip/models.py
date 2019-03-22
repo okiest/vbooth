53be8c7e5540d907485502eeafa6649f749c43a6
daniel <okonski.dan@gmail.com>
2019-03-22 17:30:17 +0000

    Add strip_code field. Add check for unique on save method.

--- strip/models.py
+++ strip/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+import uuid
+
+from django.db import models, IntegrityError
 from django.template.defaultfilters import filesizeformat
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
@@ -6,9 +8,27 @@ from django.conf import settings
 
 class PhotoStrip(models.Model):
     strip_date = models.DateTimeField(auto_now_add=True)
+    strip_code = models.CharField(blank=True, null=True, max_length=6, unique=True)
 
     def __str__(self):
-        return str(self.strip_date)
+        return str(self.strip_code)
+
+    def save(self, *arg, **kwargs):
+        if not self.strip_code:
+            self.strip_code = str(uuid.uuid4().hex[:6].upper())
+        success = False
+        errors = 0
+        while not success:
+            try:
+                super(PhotoStrip, self).save()
+            except IntegrityError:
+                errors += 1
+                if errors > 3:
+                    raise
+                else:
+                    self.strip_code = str(uuid.uuid4().hex[:6].upper())
+            else:
+                success = True
 
 
 
