bfc7ca835e3701dda469c251b5208e1884bdc1f8
daniel <okonski.dan@gmail.com>
2019-03-22 17:31:01 +0000

    Add url to view photo strip.

--- strip/urls.py
+++ strip/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url, include
 from django.urls import path
 
 from strip.views import *
+from strip.models import *
 
 urlpatterns = [
-    path('', booth, name='booth')
+    path('', booth, name='booth'),
+    path('view/<strip_code>', view_strip, name='view-strip'),
 ]
