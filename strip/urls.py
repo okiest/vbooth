814422fd3c71575f23a0ab32ccebd337ec3a74ff
daniel <okonski.dan@gmail.com>
2019-04-11 00:55:12 +0000

    Add donwload url.

--- strip/urls.py
+++ strip/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', booth, name='booth'),
     path('view/<strip_code>', view_strip, name='view-strip'),
     path('print/<strip_code>', print_strip, name='print-strip'),
+    path('download/<strip_code>', download_strip, name='download-strip'),
 ]
