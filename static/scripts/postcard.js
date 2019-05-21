26de641eae14ba1dd00cd6444fce540e7e11a6f5
daniel <okonski.dan@gmail.com>
2019-05-21 23:36:08 +0000

    Loading camera now works fullscreen on mobile and on desktop.

--- static/scripts/postcard.js
+++ static/scripts/postcard.js
@@ -3,9 +3,10 @@ const canvas = document.querySelector('canvas');
 const dummy = document.querySelector('.canvas-dummy');
 const strip = document.querySelector('.strip');
 //const snap = document.querySelector('.snap');
-const newHeight = window.outerHeight
-const newWidth = window.outerWidth
-
+var newHeight = window.outerHeight
+var newWidth = window.outerWidth
+dummy.style.width = newWidth;
+dummy.style.height = newHeight;
 
 const constraints = {
   audio: false,
@@ -33,27 +34,63 @@ function getVideo() {
 function paintToCanvas() {
   //const width = video.videoWidth;
   //const height = video.videoHeight;
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
+  //canvas.width = video.videoWidth;
+  //canvas.height = video.videoHeight;
+  canvas.width = dummy.offsetWidth;
+  canvas.height = dummy.offsetHeight;
+  console.log("Video width: " + video.videoWidth);
+  console.log("Video height: " + video.videoHeight);
+  console.log("screen width: " + newWidth);
+  console.log("screen height: " + newHeight);
+  console.log("canvas width: " + canvas.width);
+  console.log("canvas height: " + canvas.height);
+  console.log("dummy width: " + dummy.offsetWidth);
+  console.log("dummy height: " + dummy.offsetHeight);
+  //var vRatio = (canvas.height / video.videoHeight) * video.videoWidth;
+  //console.log(vRatio);
   const context = canvas.getContext('2d');
   
   return setInterval(() => {
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    //context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    //context.drawImage(video, 0, 0, newWidth, newHeight);
+    context.drawImage(video, 0, 0, dummy.offsetWidth, dummy.offsetHeight);
+    //context.drawImage(video, 0, 0,);
+    //context.drawImage(video, 0, 0, vRatio, newHeight);
+    //context.drawImage(video, 0, 0, 0, 0, 0, 0, newWidth, newHeight);
   }, 16);
 
 }
 
 function fullscreen() {
-  canvas.width = newWidth;
-  canvas.height = newHeight;
   if(dummy.webkitRequestFullScreen) {
     dummy.webkitRequestFullScreen();
   }
   else {
     dummy.mozRequestFullScreen();
-  }         
+  }
+  setTimeout(paintToCanvas, 500);
 }
 
-getVideo();
+function startCamera() {
+  getVideo();
+  var newHeight = window.outerHeight
+  var newWidth = window.outerWidth
+  canvas.width = newWidth;
+  canvas.height = newHeight;
+  //canvas.style.objectFit = "contain";
+}
 
+function queryDummy() {
+  console.log("Dummy size: " + dummy.offsetWidth, dummy.offsetHeight);
+}
+
+function queryCanvas() {
+  console.log("Canvas size: " + canvas.width, canvas.height);
+}
 video.addEventListener('canplay', paintToCanvas);
+
+window.addEventListener("orientationchange", function() {
+        var newHeight = window.outerHeight
+        var newWidth = window.outerWidth
+        fullscreen();
+}, false);
