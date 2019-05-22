823b73171ab2e919d9b69f952907a61eafa0423a
daniel <okonski.dan@gmail.com>
2019-05-22 18:52:25 +0000

    Add exit fullscreen button.

--- static/scripts/postcard.js
+++ static/scripts/postcard.js
@@ -71,6 +71,19 @@ function fullscreen() {
   setTimeout(paintToCanvas, 500);
 }
 
+function exitFull() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  paintToCanvas();
+}
+
 function startCamera() {
   getVideo();
   var newHeight = window.outerHeight
