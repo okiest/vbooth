1d5f5f85d80e2a10d5364d666fb680b156ee3b45
daniel <okonski.dan@gmail.com>
2022-05-09 19:55:52 +0000

    Changed host to localhost.

--- vbooth/settings.py
+++ vbooth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'tl!jh3xjblgh3%eon9*fy6doi=^o=&*0**gzeuzy=mmmjqxlf('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.66']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
